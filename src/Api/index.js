4604baad077387e7184061d498c7b87f470e8ef8
hamid2117 <saim008865@gmail.com>
2021-09-25 14:13:40 +0500

    This is out commit

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,10 +1,11 @@
 //* Api for Developer
 
-export const LoginApi = 'https://glacial-everglades-90519.herokuapp.com/login'
+export const LoginApi =
+  'https://glacial-everglades-90519.herokuapp.com/api/v1/login'
 export const RegisterApi =
-  'https://glacial-everglades-90519.herokuapp.com/register'
+  'https://glacial-everglades-90519.herokuapp.com/api/v1/register'
 export const ForgetPassword =
-  'https://glacial-everglades-90519.herokuapp.com/forgetPassword'
-export const Apis = 'https://glacial-everglades-90519.herokuapp.com/'
+  'https://glacial-everglades-90519.herokuapp.com/api/v1/forgetPassword'
+export const Apis = 'https://glacial-everglades-90519.herokuapp.com/api/v1/'
 
 //* Api for production
